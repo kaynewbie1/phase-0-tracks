a8138af8858642914c875bf59ccbe0b949147d90
kaynewbie1 <tenniscrazy1@gmail.com>
2017-07-22 16:08:19 -0500

    Add encrpyt coding

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,19 @@
+def encrypt(password) #names method, declares parameter
+  new_pass = ""
+  index = 0
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+  while index < password.length
+    p index_in_alpha =  alphabet.index(password[index]) #integer
+      if index_in_alpha == 25
+        new_index = 0
+      else
+        new_index = index_in_alpha + 1
+      end
+    p new_pass += alphabet[new_index]
+
+    index += 1
+  end
+  new_pass #return
+end
+
+encrypt("abc") #invoke the method
