26507b1059604d8287814ed0878a873c1ab474e9
kaynewbie1 <tenniscrazy1@gmail.com>
2017-07-23 13:43:54 -0500

    Add vampire detection questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,19 @@
+=begin
+  What is your name? Some known vampires are in the area, and we can check against the Werewolf Intelligence Bureau database for their aliases.
+=end
+puts "What is your name?"
+
+=begin
+  How old are you? What year were you born? This is to try to trick the vampire, who is likely several hundreds of years old. If an employee gives an age and a year of birth that don’t line up mathematically, that employee might be a vampire.
+=end
+puts "How old are you?"
+puts "What year were you born?"
+
+=begin
+  Our company cafeteria serves garlic bread. Should we order some for you? Vampires hate garlic. Do not even get a vampire started on garlic. Invite a vampire to an Italian restaurant even one time, and you’ll never hear the end of it.
+=end
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+=begin
+  Would you like to enroll in the company’s health insurance? Vampires are immortal, so they certainly don’t need health insurance.
+=end
+puts "Would you like to enroll in the company’s health insurance?"
