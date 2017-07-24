576954e99d0a20de7299635d09178360f911be54
kaynewbie1 <tenniscrazy1@gmail.com>
2017-07-23 22:08:39 -0500

    Update vampire.rb with test.rb code for final submission.

--- ruby/test.rb
+++ ruby/test.rb
@@ -1,47 +1,3 @@
-=begin
-age = 0
-until age.to_i == 50
-
-puts "what is your age"
-age = gets.chomp
-=end
-
-=begin
-
-rescue Exception => e
-
-end
-puts "Is your hair brown?"
-hair_brown = gets.chomp
-
-puts "is your hair short?"
-short_hair = gets.chomp
-
-
-
-
-
-=begin
-
-rescue Exception => e
-
-end
-if (age.to_i.between?(0,100)) && (hair_brown == "yes") && (short_hair == "yes")
-   puts "Happy Person"
- end
-elsif (age.to_i < 100) && (hair_brown == "no") && (short_hair = "no")
-  puts "Not Happy person"
-elsif (age.to_i != 100) && (hair_brown == "no") && (short_hair = "yes")
-  puts "Not Happy person"
-elseif (age.to_i > 100) && (hair_brown == "yes") && (short_hair = "no")
-  puts "Not Happy person"
-else
-  puts "Not Happy person"
-end
-=end
-
-
-
 puts "Hello, How many employees will be processed today?"
 employees_to_process = gets.chomp.to_i
 
@@ -98,3 +54,4 @@ health_insurance = gets.chomp
 
 end
  print "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
+#i am not sure if this is where it should go? I tried to put it before the last end and it printed, but it printed every timre the program started over.
--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,54 +1,64 @@
-=begin
-  What is your name? Some known vampires are in the area, and we can check against the Werewolf Intelligence Bureau database for their aliases.
-=end
+puts "Hello, How many employees will be processed today?"
+employees_to_process = gets.chomp.to_i
+
+until employees_to_process == 0
+
 puts "What is your name?"
 name = gets.chomp
-=begin
-  How old are you? What year were you born? This is to try to trick the vampire, who is likely several hundreds of years old. If an employee gives an age and a year of birth that don’t line up mathematically, that employee might be a vampire.
-=end
+
 puts "How old are you?"
 age = gets.chomp
 
 puts "What year were you born?"
 birth_year = gets.chomp
-=begin
-  Our company cafeteria serves garlic bread. Should we order some for you? Vampires hate garlic. Do not even get a vampire started on garlic. Invite a vampire to an Italian restaurant even one time, and you’ll never hear the end of it.
-=end
+
 puts "Our company cafeteria serves garlic bread. Should we order some for you? Please answer yes or no"
 garlic_bread = gets.chomp
-=begin
-  Would you like to enroll in the company’s health insurance? Vampires are immortal, so they certainly don’t need health insurance.
-=end
 
 puts "Would you like to enroll in the company’s health insurance? Please answer yes or no"
 health_insurance = gets.chomp
 
-#If the employee got their age right, and is willing to eat garlic bread or sign up for insurance, the result is “Probably not a vampire.”
+  if (age.to_i.between?(18,100)) && (garlic_bread == "yes") && (health_insurance == "yes")
+     puts "Probably NOT a vampire"
+  end
 
-if (age.to_i.between?(18,100)) && (garlic_bread == "yes") && (health_insurance == "yes")
-   puts "Probably NOT a vampire"
- end
+  if (age.to_i >100) && (garlic_bread == "no") && (health_insurance == "waive")
+    puts "Probably a vampire"
 
-#If the employee got their age wrong, and hates garlic bread or waives insurance, the result is “Probably a vampire.”
-if (age.to_i >100) && (garlic_bread == "no") && (health_insurance == "waive")
-  puts "Probably a vampire"
+  elsif (age.to_i >100) && (garlic_bread == "no") && (health_insurance == "no")
+    puts "Almost certainly a vampire."
+
+  elsif name == "Drake Cula" || name == "Tu Fang"
+    puts "Definitely a vampire."
+
+  else
+    puts "Results Inconclusive"
+  end
 
 
-# If the employee got their age wrong, hates garlic bread, and doesn’t want insurance, the result is “Almost certainly a vampire.”
+  puts "Please list your allergies. When list is complete, type 'done'"
+  allergies = gets.chomp
 
-elsif (age.to_i >100) && (garlic_bread == "no") && (health_insurance == "no")
-  puts "Almost certainly a vampire."
-#end
+  until allergies == "done"
+    if allergies == "sunshine"
+      puts "Probably a vampire"
+      break
+    elsif allergies != "done"
+    puts "If more allergies, list them. If finished listing allergies, type 'done'"
+    allergies = gets.chomp
+    #break
+    end
+  end
+  employees_to_process -= 1
 
-# Even if the employee is an amazing liar otherwise, anyone going by the name of “Drake Cula” or “Tu Fang” is clearly a vampire, because come on. In that case, you should print “Definitely a vampire.”
-elsif name == "Drake Cula" || name == "Tu Fang"
-  puts "Definitely a vampire."
 end
+ print "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
+#i am not sure if this is where it should go? I tried to put it before the last end and it printed, but it printed every timre the program started over.
 
 =begin
 Questions I have:
-- I think I need to set an acceptable age to check against
-- I don't know how to check the year against the birthdate entered to verify if they match
+- Wasnt sure how to verify age and/or year so I set an age range and validated against that.
+- I don't know how to check the year against the birthdate entered to verify if they match.
 - I am not sure if I honored this requirement in my code above: "Your program should base its result on the latest condition matched, not the first condition matched. In other words, it's not a "only one condition will apply" sort of scenario. This may mean repeatedly updating a variable as each condition is checked, so that the variable always contains the most precise result."
 
 =end
