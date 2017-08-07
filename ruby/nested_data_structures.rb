56595496e59ea19d4f25c86efa36390727469727
kaynewbie1 <tenniscrazy1@gmail.com>
2017-08-06 22:52:20 -0500

    Set up file to complete challenge

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,5 @@
+=begin
+In phase-0-tracks/ruby/nested_data_structures.rb, design and build a nested data structure to represent a real-world construct. You can choose the construct: Is it a highway full of cars full of people? A classroom full of desks full of supplies? A fashion show with multiple runways featuring multiple models? Build something that will use a mix of hashes and arrays.
+
+Once you've built it, print a few individual pieces of deeply nested data from the structure, showing that you know how to use multiple indexes or hash keys (or both) to access nested items. Try to demonstrate a few different types of access.
+=end
