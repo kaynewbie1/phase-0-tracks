a9e69d5ad3025a1f4fb568299db04136ccdffe90
kaynewbie1 <tenniscrazy1@gmail.com>
2017-08-09 13:26:22 -0500

    Convert string into an array

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,8 +1,22 @@
 =begin
 Release 0: Attempt a Tricky Algorithm
 In phase-0-tracks/ruby/alias_manager.rb, pseudocode and write a method that takes a spy's real name (e.g., "Felicia Torres") and creates a fake name with it by doing the following:
+=end
 
-Swapping the first and last name.
+#Swapping the first and last name.
+
+#declare a variable for the originan name
+real_name = "Felicia Torres"
+
+#swaps first and last name
+def spy_name
+  #Convert real_name_string into an to array
+  real_name = "Felicia Torres".split('')
+  #swaps first and last name
+  real_name.reverse.each { |first_name| last_name first_name}
+end
+
+=begin
 Changing all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou', and all of the consonants (everything else besides the vowels) to the next consonant in the alphabet. So 'a' would become 'e', 'u' would become 'a', and 'd' would become 'f'.
 If you get really and truly stuck, just write an alias method that loops through the original name and changes the letters somehow, without worrying about implementing this particular algorithm. It's really not worth stressing about. Simply attempting the algorithm will help you improve, even if you didn't quite get there.
 
@@ -22,4 +36,4 @@ Provide a user interface that lets a user enter a name and get a fake name back.
 Release 2: Store the Aliases
 DO THE THING
 Use a data structure to store the fake names as they are entered. When the user exits the program, iterate through the data structure and print all of the data the user entered. A sentence like "Vussit Gimodoe is actually Felicia Torres" or "Felicia Torres is also known as Vussit Gimodoe" for each agent is fine.
-end
+=end
