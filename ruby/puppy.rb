b6261f9329171fd42b81ee5fa127a3c7ce8c6329
kaynewbie1 <tenniscrazy1@gmail.com>
2017-08-10 18:57:55 -0500

    Complete 6.1

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+# SPECIES ------------------------
+# Puppy
+
+# CHARACTERISTICS ----------------
+# Energy level: high
+# Cute factor: varies
+# Friendly:
+# Name: varies
+# Color: varies
+
+# BEHAVIOR -----------------------
+# Playful
+# Nibbler
+
+
