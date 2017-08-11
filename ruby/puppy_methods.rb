e19e177d5826e4a1a2f00f900978fab81d66dd20
kaynewbie1 <tenniscrazy1@gmail.com>
2017-08-10 19:57:22 -0500

    Add Class and instance methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,58 @@ class Puppy
     toy
   end
 
+  def speak(integer)
+    integer.times do |i|
+      puts "woof"
+    end
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(integer)
+    dog_years = integer * 10
+    dog_years
+  end
+
+  def initialize
+    puts "Initializing new puppy instance ..."
+  end
+
 end
 
+fido = Puppy.new
+fido.fetch("ball")
+fido.fetch(2)
+fido.speak(4)
+fido.roll_over
+p fido.dog_years(10)
+
+
+class Cat
+
+  def initialize
+    puts "Puuuuuuurrrrrrr"
+  end
+
+  def litter_box(integer)
+   integer.times do |i|
+      puts "Make paws go to the box"
+    end
+  end
+end
+
+
+#paws.litter_box(3)
+cat_instances = []
+50.times do ||
+  paws = Cat.new
+  cat_instances << Cat.new
+  end
+
+p cat_instances
+cat_instances.each do |cat|
+  puts "cat"
+  cat.litter_box(3)
+end
