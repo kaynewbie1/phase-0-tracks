5d4904f5f81862cd2cfdecdda92cc368ac810d24
kaynewbie1 <tenniscrazy1@gmail.com>
2017-08-10 22:12:55 -0500

    Bring Santa To Life

---
+++ ruby/santa.rb
@@ -0,0 +1,21 @@
+class Santa
+
+  def speak
+    puts "Ho, ho, ho! Haaaappy holidays!"
+  end
+
+  def eat_milk_and_cookies(cookie_type)
+   puts "That was a good #{cookie_type} cookie!"
+    cookie_type
+  end
+
+  def initialize
+    puts "Initializing Santa instance ..."
+  end
+end
+
+clause = Santa.new
+clause.speak
+clause.eat_milk_and_cookies("chocolate")
+
+
