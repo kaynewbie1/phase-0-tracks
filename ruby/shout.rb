c5965a9a7024764b91d4a314cfb016a470449d47
kaynewbie1 <tenniscrazy1@gmail.com>
2017-08-13 19:42:53 -0500

    Begin work on 6.4

---
+++ ruby/shout.rb
@@ -0,0 +1,15 @@
+module Shout
+  # we'll put some methods here soon, but this code is fine for now!
+end
+
+module Shout
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+end
+
+module ShoutHappy
+  def self.yell_angrily(words)
+    words + "happy" + " :("
+  end
+end
